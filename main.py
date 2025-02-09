fe3d406a6cc4d4be6282a13aa1d867d94d6c4c11
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2025-02-09 15:27:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,22 +20,22 @@ from asyncio import TimeoutError
 from utils.file_properties import get_name, get_hash, get_media_file_size
 from c_maker import StreamBot
 
-# api_id = 28610306
-# api_hash = '3f57cc57f8883bd604baf3b814ffe023'
-# bot_token = "7580222457:AAFrfeFyi3_iHCxZbx6vvQeXt469XOqedZc"
+api_id = 28610306
+api_hash = '3f57cc57f8883bd604baf3b814ffe023'
+bot_token = "7580222457:AAFrfeFyi3_iHCxZbx6vvQeXt469XOqedZc"
 
-# BIN_CHANNEL = -1002257653178
-# BIND_ADRESS = '159.89.168.247'
+BIN_CHANNEL = -1002257653178
+BIND_ADRESS = '159.89.168.247'
 
-# PORT = 8000
+PORT = 8000
 
-# FQDN = f"{BIND_ADRESS}:{PORT}"
+FQDN = f"{BIND_ADRESS}:{PORT}"
 
-# HAS_SSL = False
-# if HAS_SSL:
-#     URL = "https://{}/".format(FQDN)
-# else:
-#     URL = "http://{}/".format(FQDN)
+HAS_SSL = False
+if HAS_SSL:
+    URL = "https://{}/".format(FQDN)
+else:
+    URL = "http://{}/".format(FQDN)
 
 # StreamBot = Client("Downl", bot_token= bot_token , api_id= api_id, api_hash= api_hash, workers=50)
 
