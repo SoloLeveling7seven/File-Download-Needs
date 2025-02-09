1a871c39c4abf23b54fb5799c79e9e9c8391cc8c
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2025-02-09 15:29:54 +0530

    Update custom_dl.py

--- utils/custom_dl.py
+++ utils/custom_dl.py
@@ -2,7 +2,7 @@
 import math
 import asyncio
 import logging
-from main import BIN_CHANNEL
+from c_maker import BIN_CHANNEL
 from typing import Dict, Union
 from pyrogram import Client, utils, raw
 from .file_properties import get_file_ids
