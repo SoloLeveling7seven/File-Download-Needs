ec4df592f3c57ef7944e916108fd994dca9a941f
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2025-02-09 17:27:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,24 +18,12 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from utils.human_readable import humanbytes
 from asyncio import TimeoutError
 from utils.file_properties import get_name, get_hash, get_media_file_size
-from c_maker import StreamBot
+from c_maker import StreamBot,URL, FQDN, BIN_CHANNEL
 
 api_id = 28610306
 api_hash = '3f57cc57f8883bd604baf3b814ffe023'
 bot_token = "7580222457:AAFrfeFyi3_iHCxZbx6vvQeXt469XOqedZc"
 
-BIN_CHANNEL = -1002257653178
-BIND_ADRESS = '159.89.168.247'
-
-PORT = 8000
-
-FQDN = f"{BIND_ADRESS}:{PORT}"
-
-HAS_SSL = False
-if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
-else:
-    URL = "http://{}/".format(FQDN)
 
 @StreamBot.on_message(filters.text & filters.command('start'))
 async def okzz(c: Client, m: Message):
