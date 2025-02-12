e30467172e16b25fb0e58cae5c1e9be517903f29
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2025-02-12 17:04:21 +0530

    Update and rename c_maker.py to config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters, idle
 
-api_id = xx
-api_hash = 'xx'
-bot_token = "xx" # your bot token
+api_id = 28610306
+api_hash = '3f57cc57f8883bd604baf3b814ffe023'
+bot_token = "7580222457:AAFrfeFyi3_iHCxZbx6vvQeXt469XOqedZc" # your bot token
 
 BIN_CHANNEL = -1002257653178 # your secret channel
-BIND_ADRESS = 'xxx' # your vps ip or domain
+BIND_ADRESS = '159.89.168.247' # your vps ip or domain
 
 PORT = 8000 #keep it same 
 
