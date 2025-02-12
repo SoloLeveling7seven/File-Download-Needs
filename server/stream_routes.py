27921ceb9cfe659eddca72704afe8f9a296b6696
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2025-02-12 17:53:03 +0530

    Update stream_routes.py

--- server/stream_routes.py
+++ server/stream_routes.py
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(_):
-    return "I am online"
+    return web.Response(text="I am online")
 
 
 @routes.get(r"/{path:\S+}", allow_head=True)
